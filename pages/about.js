bf58223d796edb5c8c274d5dbf36ea7dd632562d
s4mpl <brandanjroachell@att.net>
2022-03-27 02:09:59 -0400

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,26 @@
-const About = () => {
+import fs from 'fs';
+import path from 'path';
+
+const About = ({data}) => {
   return (
-    <div>
-      <p>about me: i am swag (i am Brandan Roachell)</p>
-    </div>
+    <>
+      <div>
+        <h1>about me:</h1>
+        i am swag (i am Brandan Roachell)
+      </div>
+      <div>
+        {data}
+      </div>
+    </>
   );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      data: fs.readFileSync(path.join('assets', 'about.md')).toString()
+    }
+  };
 };
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,14 @@
 import Link from 'next/link'
-import fs from 'fs'
-import { getProjects } from '../components/utils';
 
-const Home = ({slugs}) => {
+const Home = () => {
   return (
     <>
       <div>
+        <Link href='about'>about me</Link><br></br>
         <Link href='projects'>all projects</Link>
       </div>
     </>
   );
-};
-
-export const getStaticProps = async () => {
-  const files = fs.readdirSync('assets/projects');
-  const a = getProjects();
-
-  return {
-    props: {
-      slugs: files.map(filename => filename.replace('.md', ''))
-    }
-  };
 };
 
 export default Home;
